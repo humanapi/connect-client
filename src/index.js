15dfc0285cde7e43fb0794f94ba62b1b23d61a50
Austin Tackaberry <austin@humanapi.co>
2018-09-11 18:54:42 -0700

    Add IE11 support

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 export function open(options = {}) {
+    // IE11 polyfill to remove DOM element
+    Element.prototype.remove = function() {
+        this.parentElement.removeChild(this);
+    }
+    NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+        for(var i = this.length - 1; i >= 0; i--) {
+            if(this[i] && this[i].parentElement) {
+                this[i].parentElement.removeChild(this[i]);
+            }
+        }
+    }
+
     if (document.getElementById("human-api")) {
         return null;
     }
