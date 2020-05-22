49005583a9b39541b23a78657c47aa808958ef52
Alan Rodriguez <alanrodriguezromero@gmail.com>
2020-05-22 15:30:01 -0500

    fix(ie11): HB-2166 add childNode remove polyfill
    - This polyfill is not available in corejs since its part of the DOM

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-    entry: "./lib/index.js",
+    entry: [ "./lib/polyfills.js", "./lib/index.js" ],
     output: {
         path: path.resolve(__dirname, "build"),
         filename: "humanapi-connect-client.js",
